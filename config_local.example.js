c408769c96bff9e3ad4963e3e908dac74f6ac774
dvdzhou <undavidzhou@gmail.com>
2026-02-05 17:04:53 +0100

    Improve setup instructions in mapbox config template

--- config_local.example.js
+++ config_local.example.js
@@ -1,8 +1,8 @@
-// config_local.js
+// config_local.example.js
 
 // ISTRUZIONI:
-// 1. Rinomina questo file in "config_local.js"
+// 1. **DUPLICA** questo file e rinominalo in "config_local.js"
 // 2. Inserisci il tuo token Mapbox personale qui sotto
-// 3. Questo file "config_local.js" è già nel .gitignore, quindi non verrà mai caricato nella repo.
+// 3. Il file "config_local.js" è già nel .gitignore, quindi non verrà mai caricato nella repo.
 
 const MAPBOX_PRIVATE_TOKEN = "pk.use_your_own_token";
